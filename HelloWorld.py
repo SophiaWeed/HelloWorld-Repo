26bad3a76032e0140f5fe5c5b3c2fa1e3602f120
SophiaWeed <if3821ki@go.minnstate.edu>
2025-08-31 09:54:30 -0700

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+# Hello, my name is Sophia Weed. I'll be your god and master today.
+
+# Declare Variable (This section is optional to include for Python)
+first_name = ""
+last_name = ""
+tech_id = -1
+exp_date = ""
+
+# Create Input 
+first_name = input("Enter first name:")
+last_name = input("Enter last name:")
+tech_id = input("Enter Tech ID:")
+exp_date = input("Enter expiration date:")
+
+# Output Print
+print("Student details on MAVCard: " + first_name +  last_name + " Tech ID:" + tech_id + " Expires:" + exp_date)
+
+print(" _____")
+print("/ ' ' \\")
+print("| --- |")
+print("\\_____/")
