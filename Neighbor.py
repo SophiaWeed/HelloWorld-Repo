b92a7556782315914091721748f969b8635f7cb3
SophiaWeed <if3821ki@go.minnstate.edu>
2025-08-31 09:59:54 -0700

    Add files via upload

---
+++ Neighbor.py
@@ -0,0 +1,14 @@
+favorite_class = input("What is your favorite class?")
+print(favorite_class, "sounds pretty cool. Mine is CIS 121.")
+
+favorite_animal = input("What are your favorite animals?")
+print("I love", favorite_animal, "too! Mine are birds and fish.")
+
+print("  __________")
+print(" /    (  ]^ \\    _______")
+print("/|  ____  |\\ \\  /  ( ]^ \\ ") 
+print(" |__)| ||_) \\_\\ |_|—|_|\\/")
+
+print("    _____")
+print("\\_/____|.\\")
+print("/  \\___|_/")
